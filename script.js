e8a20fdb1d58ae5a11c690c170997073738e7534
Laura Long <lauralonggone@gmail.com>
2023-12-15 08:34:06 -0700

    Feat: Finish layout, add text decorations

--- script.js
+++ script.js
@@ -7,20 +7,19 @@ function goBack() {
   window.location.href = 'index.html'
 }
   
-  function submitForm() {
-    const name = document.getElementById("residentName") || document.getElementById("nonResidentName");
-    const address = document.getElementById("residentAddress") || null;
-    const residency = document.getElementById("residentTime") || null;
-    const zip = document.getElementById("nonResidentZip") || null;
-    const comment = document.getElementById("residentComment") || document.getElementById("nonResidentComment");
+function submitForm() {
+  const name = document.getElementById("residentName") || document.getElementById("nonResidentName");
+  const address = document.getElementById("residentAddress") || null;
+  const residency = document.getElementById("residentTime") || null;
+  const zip = document.getElementById("nonResidentZip") || null;
+  const comment = document.getElementById("residentComment") || document.getElementById("nonResidentComment");
+
+  // Log form data to console
+  console.log("Name:", name.value.trim());
+  if (address) console.log("Address:", address.value.trim());
+  if (residency) console.log("Residency:", residency.value.trim());
+  if (zip) console.log('Zip:', zip.value.trim());
+  console.log("Comment:", comment.value.trim());
   
-    // Log form data to console
-    console.log("Name:", name.value.trim());
-    if (address) console.log("Address:", address.value.trim());
-    if (residency) console.log("Residency:", residency.value.trim());
-    if (zip) console.log('Zip:', zip.value.trim());
-    console.log("Comment:", comment.value.trim());
-    
-  
-  }
-  
+
+}
