c08eccc69a1b3b36ce8294a6524be1f53b0cfb4f
Laura Long <lauralonggone@gmail.com>
2024-07-03 20:33:20 -0600

    Fix redirect by adding the script tag that got lost in a merge conflict

--- script.js
+++ script.js
@@ -1,20 +1,28 @@
 function redirectToForm(type) {
-    const url = type === 'resident' ? 'resident.html' : 'nonResident.html';
-    window.location.href = url;
-  }
+  const url = type === 'resident' ? 'resident.html' : 'nonResident.html';
+  window.location.href = url;
+}
 
 function goBack() {
-  window.location.href = 'index.html'
+window.location.href = 'index.html'
 }
-  
+
 function submitForm() {
-  console.log('Do something cool!!!')
+console.log('Do something cool!!!')
 
-  console.log("Name:", name.value.trim());
-  if (address) console.log("Address:", address.value.trim());
-  if (residency) console.log("Residency:", residency.value.trim());
-  if (zip) console.log('Zip:', zip.value.trim());
-  console.log("Comment:", comment.value.trim());
+// I was trying to console log data from the form...
+
+// const name = document.getElementById("residentName") || document.getElementById("nonResidentName");
+// const address = document.getElementById("residentAddress") || null;
+// const residency = document.getElementById("residentTime") || null;
+// const zip = document.getElementById("nonResidentZip") || null;
+// const comment = document.getElementById("residentComment") || document.getElementById("nonResidentComment");
+
+// console.log("Name:", name.value.trim());
+// if (address) console.log("Address:", address.value.trim());
+// if (residency) console.log("Residency:", residency.value.trim());
+// if (zip) console.log('Zip:', zip.value.trim());
+// console.log("Comment:", comment.value.trim());
+
+
 }
-
-console.log('hello world')
